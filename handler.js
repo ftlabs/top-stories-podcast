9a9844b1c5c90aace2ae7f2be9c3b3840c53d17f
Jamie Brown <jamiebrown201@hotmail.com>
2019-12-02 15:59:12 +0000

    add handler

--- handler.js
+++ handler.js
@@ -1,18 +1,62 @@
-'use strict';
+"use strict";
+const parser = require("./lib/parser");
+const textToAudio = require("./lib/textToAudio");
+const db = require("./lib/db");
 
-module.exports.hello = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+module.exports.get = async event => {
+  try {
+    const url = event.queryStringParameters.url;
+    const collection = event.queryStringParameters.collection;
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    const articleText = await parser(url);
+    const {
+      ssl,
+      title,
+      byline,
+      excerpt,
+      length,
+      dir,
+      source,
+      content
+    } = articleText;
+    const resultAudio = await textToAudio(ssl);
+    const { TaskId, OutputUri } = resultAudio.SynthesisTask;
+    const resultDb = await db({
+      id: TaskId,
+      source,
+      collection,
+      audio: OutputUri,
+      content,
+      ssl,
+      length,
+      title,
+      excerpt,
+      byline,
+      dir
+    });
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: resultDb,
+          input: event
+        },
+        null,
+        2
+      )
+    };
+  } catch (err) {
+    console.log(JSON.stringify(err));
+    return {
+      statusCode: 401,
+      body: JSON.stringify(
+        {
+          message: JSON.stringify(err),
+          input: event
+        },
+        null,
+        2
+      )
+    };
+  }
 };
